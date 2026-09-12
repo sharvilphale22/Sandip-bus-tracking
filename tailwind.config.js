7cb0bd1d60fec4507ac991542a776c67c63ee319
sharvil phale <phalesharvil390@gmail.com>
2026-09-12 19:22:19 +0530

    Update Tailwind CSS content paths for configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  "./index.html",
+  "./*.{js,jsx,ts,tsx}",
+  "./components/**/*.{js,jsx,ts,tsx}",
+  "./pages/**/*.{js,jsx,ts,tsx}",
+  "./context/**/*.{js,jsx,ts,tsx}",
+  "./utils/**/*.{js,jsx,ts,tsx}",
+],
   theme: {
     extend: {
       colors: {
