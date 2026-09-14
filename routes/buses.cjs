13fb56738302980f6979b91f1cd7d97266870ce3
sharvil phale <phalesharvil390@gmail.com>
2026-09-14 11:19:09 +0530

    Update buses.cjs

--- routes/buses.cjs
+++ routes/buses.cjs
@@ -1,5 +1,5 @@
 const express = require('express');
-const { verifyToken, requireRole } = require('../middleware/auth');
+const { verifyToken, requireRole } = require('../middleware/auth.cjs');
 const { buses, drivers, students, generateId } = require('../models/data.cjs');
 
 const router = express.Router();
