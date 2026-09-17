4960f2b8bdf5a8455b528c69fafb661869ac48e9
sharvil phale <phalesharvil390@gmail.com>
2026-09-17 22:45:33 +0530

    Update MapView.jsx

--- components/MapView.jsx
+++ components/MapView.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import { LocateFixed, Loader2, MapPinOff } from 'lucide-react';
 
